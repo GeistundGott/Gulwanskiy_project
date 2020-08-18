a219b67e2e9cbb8b6cd8821d1a1d0432875e2371
GeistundGott <gau1984@rambler.ru>
2020-08-18 21:36:31 +0300

    добавил преобразование строки в квадраты чисел

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Gulwanskiy");
+		System.out.println("Gulwanskiy");
+		String reg = "(([0-9]{1,3}), )+([0-9]{1,3})?";
+		String s="5, 11, 2, 25, 13, 14";
+		if(Pattern.matches(reg, s)) {
+			String[] sarr=s.split(", ");
+			int[] arr=new int[sarr.length];
+			for(int i=0; i<sarr.length; i++) {
+				arr[i]=(int)Math.pow(Integer.parseInt(sarr[i]),2);
+			}
+			System.out.println(Arrays.toString(sarr)+" исходная строка");
+			System.out.println(Arrays.toString(arr)+" квадрат исходной строки");
+		}else {
+			System.out.println("что-то пошло не так");
+		}
 	}
 
 }
